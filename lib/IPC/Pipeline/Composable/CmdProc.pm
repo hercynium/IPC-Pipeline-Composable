4148303720e7df92be4b6ea7c914e32f46e4505d
Stephen R. Scaffidi <stephen@scaffidi.net>
2012-05-17 16:31:38 -0400

    command subst. working, I think...

--- lib/IPC/Pipeline/Composable/CmdProc.pm
+++ lib/IPC/Pipeline/Composable/CmdProc.pm
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-package IPC::Composable::Pipeline::CmdProc;
+package IPC::Pipeline::Composable::CmdProc;
 # ABSTRACT: A command process in a pipeline
 use Carp;
 use English qw( -no_match_vars );
@@ -9,6 +9,7 @@ use Scalar::Util qw( reftype blessed );
 use File::Temp qw( tmpnam );
 use POSIX qw( mkfifo );
 use IPC::Pipeline::Continuous qw( pipeline_c pipeline );
+use Data::Dumper;
 
 =method new
 
@@ -24,8 +25,9 @@ Takes the following options:
 sub new {
   my ($class, %opt) = @_;
   return bless {
-    procsub_class     => 'IPC::Pipeline::Continuous::ProcSub',
-    placeholder_class => 'IPC::Pipeline::Continuous::Placeholder',
+    cmdsub_class      => __PACKAGE__,
+    procsub_class     => 'IPC::Pipeline::Composable::ProcSub',
+    placeholder_class => 'IPC::Pipeline::Composable::Placeholder',
     %opt,
   }, $class;
 }
@@ -56,6 +58,13 @@ sub spec {
   my ($self) = @_;
   return [ $self->cmd, $self->args ];
 }
+
+=attribute cmdsub_class
+
+The class this object recognizes for command substitution
+
+=cut
+sub cmdsub_class { shift->{cmdsub_class} }
 
 =attribute procsub_class
 
@@ -91,13 +100,33 @@ sub run {
   my %procsub_pipes; # hold named pipe paths for process substitution
 
   my @args = map { $self->_process_arg($_, \%procsub_pipes, \%opt) } $self->args;
-return @args;
-  my @cmd_pids     = pipeline_c( @opt{qw(stdin stdout stderr)}, [$self->cmd, @args] );
+  my @handles      = @opt{qw(stdin stdout stderr)};
+  my $cmd_spec     = [$self->cmd, @args];
+  #print Dumper(\@handles);
+  #print Dumper($cmd_spec);
+  my @cmd_pids     = $self->_run_pipeline( @handles, $cmd_spec );
   my @procsub_pids = __do_procsubs( \%procsub_pipes, $opt{stderr} );
   my @pids         = ( @cmd_pids, @procsub_pids );
   return wantarray ? @pids : \@pids;
 }
 
+sub _run_pipeline {
+  my ($self, $stdin, $stdout, $stderr, @cmds) = @_;
+
+  #print Dumper [$stdin, $stdout, $stderr, @cmds];
+
+  # TODO: when this is working for stdout, do it for all three handles.
+  if ( ref($stdout) and reftype($stdout) eq 'SCALAR' ) {
+    pipe(my($mine), my($theirs)); # TODO: file a bug on autodie for this.
+    my @pids = pipeline_c( $stdin, $theirs, $stderr, @cmds );
+    wait for @pids;
+    close $theirs;
+    while (sysread($mine, my $buf, 512)) { $$stdout .= $buf; };
+    close $mine;
+    return @pids;
+  }
+  return pipeline_c( $stdin, $stdout, $stderr, @cmds );
+}
 
 sub _process_arg {
   my ($self, $arg, $procsub_pipes, $opt) = @_;
@@ -120,6 +149,13 @@ sub _process_arg {
     return $self->_process_arg($actual, $procsub_pipes, $opt);
   }
 
+  if ( $arg->isa( $self->cmdsub_class ) ) {
+    my $buf;
+    my %cs_opt = %$opt;
+    delete @cs_opt{qw(stdin stdout stderr)};
+    my @pids = $arg->run(%cs_opt, stdout => \$buf);
+    return $buf;
+  }
 }
 
 sub __do_procsubs {
