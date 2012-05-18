fcfa0c8726ee7df4f2bcf56e13b5569746f5985f
Stephen R. Scaffidi <stephen@scaffidi.net>
2012-05-18 14:15:04 -0400

    made the newest test script simpler

--- test-placeholders.pl
+++ test-placeholders.pl
@@ -10,33 +10,21 @@ use lib 'lib';
 use IPC::Pipeline::Composable::CmdProc;
 use IPC::Pipeline::Composable::Placeholder;
 
-my $ph1 = IPC::Pipeline::Composable::Placeholder->new(name => 'foo');
-my $ph2 = IPC::Pipeline::Composable::Placeholder->new(name => 'bar');
-my $cmd1 = IPC::Pipeline::Composable::CmdProc->new(cmd => 'head', args => ['-n1', $ph1]);
-my $cmd2 = IPC::Pipeline::Composable::CmdProc->new(cmd => 'echo', args => ['-n', $ph2]);
+my $ph1 = IPC::Pipeline::Composable::Placeholder->new(name => 'foo'); # ipc_ph('foo')
+my $ph2 = IPC::Pipeline::Composable::Placeholder->new(name => 'bar'); # ipc_ph('bar')
+my $cmd1 = IPC::Pipeline::Composable::CmdProc->new(cmd => 'head', args => ['-n1', $ph1]); # ipc_cmd(...)
+my $cmd2 = IPC::Pipeline::Composable::CmdProc->new(cmd => 'echo', args => ['-n', $ph2]);  # ipc_cmd(...)
 
 my $expected = (read_file $0)[0];
 
 my ($stdout1, $stderr1, @pids1) = capture {
-    pipe my($read, $write);
-    my @pids = $cmd1->run(foo=>$0, stdout => $write);
-    wait for @pids;
-    close $write;
-    while (sysread($read, my $buf, 512)) { print $buf }
-    close $read;
-    return @pids;
+    map { wait } $cmd1->run(foo=>$0, stdout => \*STDOUT);
 };
 
 is $stdout1, $expected, "running simple command";
 
 my ($stdout2, $stderr2, @pids2) = capture {
-    pipe my($read, $write);
-    my @pids = $cmd2->run(foo=>$0, bar=>$cmd1, stdout => $write);
-    wait for @pids;
-    close $write;
-    while (sysread($read, my $buf, 512)) { print $buf }
-    close $read;
-    return @pids;
+    map { wait } $cmd2->run(foo=>$0, bar=>$cmd1, stdout => \*STDOUT);
 };
 
 is $stdout2, $expected, "running command with command substitution and placeholder";
